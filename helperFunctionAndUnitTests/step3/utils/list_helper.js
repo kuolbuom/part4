cdd144da077c0f22fd6f339ba3c702a2a7209d28
kuol buom <kuolbuom111@gmail.com>
2026-05-19 09:46:20 +0300

    Helper Functions and Unit Tests, step 4

---
+++ helperFunctionAndUnitTests/step3/utils/list_helper.js
@@ -0,0 +1,19 @@
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  let favorite = blogs[0]
+
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes) {
+      favorite = blog
+    }
+  })
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
+module.exports = {favoriteBlog}

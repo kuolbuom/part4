1476099c18e8e20657ef42c5340122373a66db3a
kuol buom <kuolbuom111@gmail.com>
2026-05-22 09:25:52 +0300

    Blog List Expansion, step 8

---
+++ blogListExpansion/step8/controllers/blogs.js
@@ -0,0 +1,98 @@
+const jwt = require('jsonwebtoken')
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const logger = require('../utils/logger')
+const config = require('../utils/config')
+
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+   .find({})
+   .populate('user', {
+      username: 1,
+      name: 1
+    })
+  response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
+const decodedToken = jwt.verify(
+  request.token,
+  config.SECRET
+)
+
+if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+ if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', (request, response) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(blog => {
+      response.status(204).end()
+    })
+    .catch(logger.error)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    blog,
+    { new: true, 
+      runValidators: true, 
+      context: 'query'
+     }
+  )
+
+  if (updatedBlog) {
+    response.json(updatedBlog)
+  } else {
+    response.status(404).end()
+  }
+})
+module.exports = blogsRouter

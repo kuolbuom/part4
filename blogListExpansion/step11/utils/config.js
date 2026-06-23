a87f9749d47a47b78ad9f816bbf1b00319d1dd87
kuol buom <kuolbuom111@gmail.com>
2026-06-23 09:58:28 +0300

    configure testing

--- blogListExpansion/step11/utils/config.js
+++ blogListExpansion/step11/utils/config.js
@@ -8,6 +8,9 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
+console.log('ENV:', process.env.NODE_ENV)
+console.log('DATABASE:', MONGODB_URI)
+
 module.exports = {
   MONGODB_URI,
   PORT,
